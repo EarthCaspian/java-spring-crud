4ae5d8f91ab4499815c2783fac11394aa99d7f9b
EarthCaspian <hazar.a@outlook.com>
2024-01-25 22:44:42 +0300

    authorities type change integrated into register method

--- src/main/java/com/java_spring/java_spring_crud/services/concretes/UserManager.java
+++ src/main/java/com/java_spring/java_spring_crud/services/concretes/UserManager.java
@@ -1,7 +1,9 @@
 package com.java_spring.java_spring_crud.services.concretes;
 
+import com.java_spring.java_spring_crud.entities.RoleEntity;
 import com.java_spring.java_spring_crud.entities.User;
 import com.java_spring.java_spring_crud.repositories.UserRepository;
+import com.java_spring.java_spring_crud.services.abstracts.RoleService;
 import com.java_spring.java_spring_crud.services.abstracts.UserService;
 import com.java_spring.java_spring_crud.services.dtos.user.CreateUserRequest;
 import com.java_spring.java_spring_crud.services.dtos.user.LoginRequest;
@@ -10,6 +12,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -18,13 +25,19 @@ public class UserManager implements UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final RoleService roleService;
 
     @Override
     public void register(CreateUserRequest createUserRequest) {
+        Set<RoleEntity> authorities = createUserRequest.getRoles().stream()
+                .map(roleService::findByName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
             User user = User.builder()
                     .username(createUserRequest.getUsername())
                     .email(createUserRequest.getEmail())
-                    .authorities(createUserRequest.getRoles())
+                    .authorities(authorities)
                     .password(passwordEncoder.encode(createUserRequest.getPassword()))
                     .build();
             userRepository.save(user);

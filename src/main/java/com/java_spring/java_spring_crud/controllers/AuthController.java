44616dce73b506f106387d8dba242dea48dc4023
EarthCaspian <hazar.a@outlook.com>
2024-02-08 21:06:55 +0300

    authController added

---
+++ src/main/java/com/java_spring/java_spring_crud/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.java_spring.java_spring_crud.controllers;
+
+
+import com.java_spring.java_spring_crud.services.abstracts.AuthService;
+import com.java_spring.java_spring_crud.services.dtos.user.CreateUserRequest;
+import com.java_spring.java_spring_crud.services.dtos.user.LoginRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private AuthService authService;
+
+
+    @PostMapping("register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody CreateUserRequest request) {
+        authService.register(request);
+    }
+
+    @PostMapping("login")
+    @ResponseStatus(HttpStatus.OK)
+    public String login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+
+    }
+}

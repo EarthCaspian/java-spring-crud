e41479a412e4cb73bc50791d8bd673482c8fb252
EarthCaspian <hazar.a@outlook.com>
2024-03-30 16:30:59 +0300

    Exception types added

---
+++ src/main/java/com/java_spring/java_spring_crud/core/utilities/exceptions/types/BusinessException.java
@@ -0,0 +1,7 @@
+package com.java_spring.java_spring_crud.core.utilities.exceptions.types;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/java_spring/java_spring_crud/core/utilities/exceptions/types/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.java_spring.java_spring_crud.core.utilities.exceptions.types;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException (String message) {super(message);}
+}

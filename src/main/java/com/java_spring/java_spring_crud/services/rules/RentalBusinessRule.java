0e1d88f2f88fcf61cefa5f059db2d565cb0b2762
EarthCaspian <hazar.a@outlook.com>
2024-04-03 21:11:17 +0300

    business rule conditional methods for rentals

---
+++ src/main/java/com/java_spring/java_spring_crud/services/rules/RentalBusinessRule.java
@@ -0,0 +1,37 @@
+package com.java_spring.java_spring_crud.services.rules;
+
+import com.java_spring.java_spring_crud.core.utilities.exceptions.types.BusinessException;
+import com.java_spring.java_spring_crud.core.utilities.messages.MessageService;
+import com.java_spring.java_spring_crud.repositories.RentalRepository;
+import com.java_spring.java_spring_crud.services.constants.Messages;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@AllArgsConstructor
+public class RentalBusinessRule {
+    private final RentalRepository rentalRepository;
+    private final MessageService messageService;
+
+    public void existsRentalById(int id) {
+        if (!rentalRepository.existsById(id)) {
+            throw new BusinessException(messageService.getMessage(Messages.Rental.rentalNotFoundMessage));
+        }
+    }
+
+    public void checkStartDate(LocalDate startDate) {
+        if (startDate.isBefore(LocalDate.now())) {
+            throw new BusinessException(messageService.getMessage(Messages.Rental.rentalStartDateError));
+        }
+    }
+
+    public void checkEndDate(LocalDate startDate, LocalDate endDate) {
+        if (endDate.isBefore(startDate)) {
+            throw new BusinessException(messageService.getMessage(Messages.Rental.rentalEndDateError));
+        }
+    }
+
+
+}
